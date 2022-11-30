040e18d6d9e0545aa54084f8c98dc68545586bd4
Sankar-Mutyala <mutyalasivasankar7@gmail.com>
2022-11-30 15:27:05 +0530

    logincompleted

--- propalin.js
+++ propalin.js
@@ -1,1 +1,13 @@
-let bag=""
+let str="sankar"
+let bag="";
+let N=6;
+    
+    for(let i=N-1; i>=0; i--){
+        bag=bag+str;
+    }
+    if(bag==str){
+      console.log("Yes");
+  }
+  else{
+      console.log("No");
+  }
